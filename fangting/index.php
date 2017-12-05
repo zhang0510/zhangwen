16332b55def99b4a2cf0e049ef3dcc0fbbbfaf81
zhangwen <18514430012@163.com>
2017-12-05 15:40:32 +0800

    提交

---
+++ fangting/index.php
@@ -0,0 +1,334 @@
+﻿<!DOCTYPE html>
+<html  xmlns="http://www.w3.org/1999/xhtml"  xml:lang="en"  lang="en">
+<head>
+<title>方婷</title>
+<meta name="keywords" content="在乎你，ft我很在乎你">
+<meta name="description" content="  这里是个人的表白页面 ">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+  <link rel="shortcut icon" href="index/images/favicon.ico">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+  <script type="text/javascript" src="js/jquery.min.js" ></script>
+  <script type="text/javascript" src="js/content_switch.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+  <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/cScroll.js"></script>
+  <script src="js/jquery.cycle.all.latest.js" type="text/javascript"></script>
+  <script src="js/jquery.color.js" type="text/javascript"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+  <script type="text/javascript" src="js/functions.js"></script>
+  <script src="js/prettyPhoto.js"></script>
+  <!--[if lt IE 9]>
+  	<script type="text/javascript" src="./js/html5.js"></script>
+    <link rel="stylesheet" href="./css/ie.css" type="text/css" media="all">
+  <![endif]-->
+	<!--[if lt IE 8]>
+		<div style=' clear: both; text-align:center; position: relative;'>
+			<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a>
+		</div>
+	<![endif]-->
+	<body onpaste="return false" ondragstart="return false" onmouseover="window.status='';return true" oncopy="return false;">
+	<frameset> <frame src="index.php"> </frameset>
+<script> 
+/*function stop(){
+alert('ft，我爱你！');
+return false; 
+}*/
+document.oncontextmenu=stop; 
+</script>
+<body onselectstart="return false" onpaste="return false" oncopy="return false;" 
+oncut="return false;" >
+</a>
+    <script>
+	
+        function checkhtml5()
+        {
+            if ($.browser.msie  && parseInt($.browser.version, 10) < 9) {
+                document.body.innerHTML="<div style=' clear: both; text-align:center; position: relative; height: 50px; margin-top: 30px; padding: 20px; background-color: red; color: white; font-size:20px;'>你的浏览器非常落后，不支持 HTML5!<br/>请使用 Chrome 14+/IE 9+/Firefox 7+/Safari 4+ 其中任意一款浏览器访问此页面。</a></div>";
+
+            }
+        }
+    </script>
+    <style>
+        a.wb_sina {
+            float:left;
+            margin-top:20px;
+            margin-left:15px;
+            display:inline-block;
+            padding:4px 10px;
+            border-radius:3px;
+            background-color:#e55345;
+            background-image:-moz-linear-gradient(top,#e96249,#e03c40);
+            background-image:-ms-linear-gradient(top,#e96249,#e03c40);
+            background-image:-webkit-gradient(linear,0 0,0 100%,from(#e96249),to(#e03c40));
+            background-image:-webkit-linear-gradient(top,#e96249,#e03c40);
+            background-image:-o-linear-gradient(top,#e96249,#e03c40);
+            background-image:linear-gradient(top,#e96249,#e03c40);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e96249',endColorstr='#e03c40',GradientType=0);
+            background-repeat:repeat-x;
+            text-shadow:0 -1px 0 rgba(0,0,0,.5);
+            border:1px solid #cf2b28;
+            color:#fff!important;
+            box-shadow:0 1px 0 rgba(255,255,255,.2) inset,0 1px 0 rgba(0,0,0,.2);
+        }
+        a.wb_sina:hover {
+            background-image:-moz-linear-gradient(top,#e03c40,#e96249);
+            background-image:-ms-linear-gradient(top,#e03c40,#e96249);
+            background-image:-webkit-gradient(linear,0 0,0 100%,from(#e03c40),to(#e96249));
+            background-image:-webkit-linear-gradient(top,#e03c40,#e96249);
+            background-image:-o-linear-gradient(top,#e03c40,#e96249);
+            background-image:linear-gradient(top,#e03c40,#e96249);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e03c40',endColorstr='#e96249',GradientType=0);
+        }
+        a.wb_sina span {
+            display:inline-block;
+            vertical-align:-5px;
+            margin-right:7px;
+            height:20px;
+            width:24px;
+            background:url(./images/weibo.png) no-repeat;
+        }
+        a.wb_tencent {
+            float:left;
+            margin-top:20px;
+            margin-left:15px;
+            display:inline-block;
+            padding:4px 10px;
+            border-radius:3px;
+            background-color:#0e7fcc;
+            background-image:-moz-linear-gradient(top,#1288d4,#0771c1);
+            background-image:-ms-linear-gradient(top,#1288d4,#0771c1);
+            background-image:-webkit-gradient(linear,0 0,0 100%,from(#1288d4),to(#0771c1));
+            background-image:-webkit-linear-gradient(top,#1288d4,#0771c1);
+            background-image:-o-linear-gradient(top,#1288d4,#0771c1);
+            background-image:linear-gradient(top,#1288d4,#0771c1);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#1288d4',endColorstr='#0771c1',GradientType=0);
+            background-repeat:repeat-x;
+            text-shadow:0 -1px 0 rgba(0,0,0,.5);
+            border:1px solid #0D6EB8;
+            color:#fff!important;
+            box-shadow:0 1px 0 rgba(255,255,255,.2) inset,0 1px 0 rgba(0,0,0,.2);
+        }
+        a.wb_tencent:hover {
+            background-color:#0e7fcc;
+            background-image:-moz-linear-gradient(top,#0771c1,#1288d4);
+            background-image:-ms-linear-gradient(top,#0771c1,#1288d4);
+            background-image:-webkit-gradient(linear,0 0,0 100%,from(#0771c1),to(#1288d4));
+            background-image:-webkit-linear-gradient(top,#0771c1,#1288d4);
+            background-image:-o-linear-gradient(top,#0771c1,#1288d4);
+            background-image:linear-gradient(top,#0771c1,#1288d4);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#0771c1',endColorstr='#1288d4',GradientType=0);
+        }
+        a.wb_tencent span {
+            display:inline-block;
+            vertical-align:-5px;
+            margin-right:7px;
+            height:20px;
+            width:24px;
+            background:url(./images/weibo.png) no-repeat 0 -20px;
+        }
+        #abox
+        {
+            position: fixed;
+            _position: absolute;
+            right: 15px;
+            z-index: 99999999;
+        }
+    </style>
+</head>
+
+<body onLoad="checkhtml5()">
+<div id="abox">
+</div>
+<div class="page_spinner">
+    <div></div>
+</div>
+<div class="over">
+	<div class="centre">
+		<div class="main">
+			<!--header -->
+	  <header>
+          <h1><a target="_blank" href="./love520/index.html"><span id="logo"><img src="images/logo1.jpg" onclick="biaobai()"></span></a></h1>
+				<nav class="menu">
+					<ul id="menu">
+						<li id="nav1"><img src="images/nav1.png" alt=""><span>爱情</span></li>
+						<li id="nav2"><img src="images/nav2.png" alt=""><span>记事</span></li>
+						<li id="nav3"><img src="images/nav3.png" alt=""><span>絮叨</span></li>
+						<li id="nav4"><img src="images/nav4.png" alt=""><span>留言</span></li>
+						<li id="nav5"><img src="images/nav5.png" alt=""><span>相册</span></li>
+						<li id="nav6"><img src="images/nav6.png" alt=""><span>沙漏</span></li>
+					</ul>
+				</nav>
+				<img src="images/spacer.gif" alt="" id="navigation" usemap="#navigation">
+				<map name="navigation" class="navigation">
+	    </map>
+				<map name="logo" class="map_logo"></map>
+			</header>
+			<!--header end-->
+			<!--content -->
+			<map name="back" class="map_back"></map>
+			<article id="content">
+				<ul>
+					<li id="page_Home">
+                        <img src="images/bg_content.png" alt="" class="bg_cont">
+                        <span class="back"><img src="images/nav1.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+							<h2>爱情</h2>
+                            <div class="relative">
+                                <div class="scroll">
+                                    爱情是什么，也许我不懂<br />
+                                    但是我知道，爱一个人愿意去包容对方的一切。<br />
+                                    也许，我不会说话，总惹你不开心；<br />
+                                    也许，你听过许多的甜言蜜语；<br />
+                                    但，在美丽的承诺，也只是一个美丽的泡影。<br/>
+                                    我不知道能为你做什么，但是，我知道一定会一直对你好。
+                                </div>
+                            </div>
+                        </div>
+					</li>
+					<li id="page_About">
+						<img src="images/bg_content.png" alt="" class="bg_cont">
+						<span class="back"><img src="images/nav2.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+							<h2>记事</h2>
+							<div class="relative">
+								<div class="scroll">
+                                    <span>从2015-09-20年至今</span>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我们已相爱<?php echo floor( ( time() - strtotime("2015-09-20") )/ (60*60*24) ); ?>天</p>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;距离方婷生日还有<?php echo ceil( ( strtotime("2018-03-24") - time() )/ (60*60*24) ); ?>天</p>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;距离张文生日还有<?php echo ceil( ( strtotime("2018-06-23") - time() )/ (60*60*24) ); ?>天</p>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;距离三周年还有<?php echo ceil( ( strtotime("2018-09-20") - time() )/ (60*60*24) ); ?>天</p>
+                                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;两周年已经<?php echo floor( ( time() - strtotime("2017-09-20") )/ (60*60*24) ); ?>天</p>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li id="page_Talk">
+						<img src="images/bg_content.png" alt="" class="bg_cont">
+						<span class="back"><img src="images/nav3.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+							<h2>絮叨</h2>
+                            <div class="relative">
+                                <div class="scroll">
+                                    <div style="width:100%;float: left">等待更新<br/><span style="float: right"> --2015/4/*</span></div>
+                                </div>
+                            </div>
+						</div>
+					</li>
+					<li id="page_Message">
+						<img src="images/bg_content.png" alt="" class="bg_cont">
+						<span class="back"><img src="images/nav4.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+							<h2>留言</h2>
+                            <div class="relative">
+                                <div class="scroll">
+
+                                    <div style="min-height: 300px; padding-bottom: 50px;">
+<!-- 多说评论框 start -->
+	<div class="ds-thread" data-thread-key="#" data-title="#" data-url="love.17cnzz.com"></div>
+<!-- 多说评论框 end -->
+<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+<script type="text/javascript">
+var duoshuoQuery = {short_name:"17gmelove"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
+	</script>
+<!-- 多说公共JS代码 end -->
+                                    </div>
+                                </div>
+                            </div>
+						</div>
+					</li>
+					<li id="page_Blog">
+						<img src="images/bg_content.png" alt="" class="bg_cont">
+						<span class="back"><img src="images/nav5.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+							<h2>相册</h2>
+							<div class="relative">
+								<div class="scroll">
+									<ul class="gallery fancybox">
+                                        <li style="line-height:120px"><a href="images/uploadimage/1.jpg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/1.jpg" alt=""></a></li>
+                                        <li style="line-height:120px"><a href="images/uploadimage/2.jpg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/2.jpg" alt=""></a></li>
+                                        <li style="line-height:120px"><a href="images/uploadimage/3.jpg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/3.jpg" alt=""></a></li>
+                                        <li style="line-height:120px"><a href="images/uploadimage/4.jpg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/4.jpg" alt=""></a></li>
+                                        <li style="line-height:120px"><a href="images/uploadimage/5.jpeg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/5.jpeg" alt=""></a></li>
+                                        <li style="line-height:120px"><a href="images/uploadimage/6.jpg" rel="prettyPhoto[gallery1]"><img src="images/uploadimage/6.jpg" alt=""></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li id="page_Time">
+						<img src="images/bg_content.png" alt="" class="bg_cont">
+						<span class="back"><img src="images/nav6.png" alt="" usemap="#back"><a href="index.php#close">返回</a></span>
+						<div class="pad">
+<h2>沙漏</h2>
+                            <div id="loveHeart" style="margin-top: 30px;font-size: 25px;">
+                                        <span style="">你知道我爱你爱了多久了吗？</span>
+                                        <div id="elapseClock" style="margin: 10px 0px 10px 0px;"></div>
+                                <img src="images/1.gif" />　　　　<img src="images/2.gif" /><br/><br/>
+                                    <div id="loveu">
+                                        爱：从2015-9-20开始！<br/>
+                                        <div class="signature" style="float: right; margin-right: 50px;">by  </div>
+                                    </div>
+                          </div>
+                      </div>
+				  </div>
+					</li>
+					
+					
+				</ul>
+			</article>
+			<!--content end-->
+  </div>
+</div>
+	
+</div>
+<script>
+$(window).load(function() {	
+	$('.page_spinner').fadeOut();
+	$('body').css({overflow:'visible'});
+})
+</script>
+
+<script type="text/javascript">//修改时光沙漏时间
+    var offsetX = $("#loveHeart").width() / 2;
+    var offsetY = $("#loveHeart").height() / 2 - 55;
+    var together = new Date();
+    together.setFullYear(2015, 08, 20);
+    together.setHours(14);
+    together.setMinutes(0);
+    together.setSeconds(0);
+    together.setMilliseconds(0);
+
+        setTimeout(function () {
+            adjustWordsPosition();
+            startHeartAnimation();
+        }, 3000);
+
+        timeElapse(together);
+        setInterval(function () {
+            timeElapse(together);
+        }, 500);
+
+        adjustCodePosition();
+        $("#code").typewriter();
+</script>
+    <audio id="bgmMusic" src="http://qzone.haoduoge.com/music/A331AQ9C1TC529DFC33E0C8076611D1F5414D.mp3" preload="auto" type="audio/mp3" autoplay loop></audio>
+
+<!--coded by koma-->
+
+<!--LIVEDEMO_00 -->
+</div>
+
+<img src="images/bg_content.png" alt="" class="bg_cont">
+</body>
+</html>
---
+++ fangting/uplode.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangwen
+ * Date: 2017/12/5
+ * Time: 11:35
+ */
